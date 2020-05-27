5b6f1ea3b8c652764197fd1ee1f56d7cf3007741
Rosa <rosaareyesc@gmail.com>
2020-05-27 21:13:33 +0200

    Add login, register and forgot password pages

--- index.js
+++ index.js
@@ -1,53 +1,11 @@
-
-
-
-  (function($) {
-    "use strict"; // Start of use strict
-
-    // Toggle the side navigation
-    $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
-      $("body").toggleClass("sidebar-toggled");
-      $(".sidebar").toggleClass("toggled");
-      if ($(".sidebar").hasClass("toggled")) {
-        $('.sidebar .collapse').collapse('hide');
-      };
-    });
-
-    // Close any open menu accordions when window is resized below 768px
-    $(window).resize(function() {
-      if ($(window).width() < 768) {
-        $('.sidebar .collapse').collapse('hide');
-      };
-    });
-
-    // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-    $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
-      if ($(window).width() > 768) {
-        var e0 = e.originalEvent,
-          delta = e0.wheelDelta || -e0.detail;
-        this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-        e.preventDefault();
-      }
-    });
-
-    // Scroll to top button appear
-    $(document).on('scroll', function() {
-      var scrollDistance = $(this).scrollTop();
-      if (scrollDistance > 100) {
-        $('.scroll-to-top').fadeIn();
-      } else {
-        $('.scroll-to-top').fadeOut();
-      }
-    });
-
-    // Smooth scrolling using jQuery easing
-    $(document).on('click', 'a.scroll-to-top', function(e) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-        scrollTop: ($($anchor.attr('href')).offset().top)
-      }, 1000, 'easeInOutExpo');
-      e.preventDefault();
-    });
+    function Validate() {
+        var password = $("password1").value;
+        var confirmPassword = $("password2").value;
+        if (password != confirmPassword) {
+            $("#password2").removeClass("is-valid").addClass("is-invalid");
+        }
+        $("#password2").removeClass("is-invalid").addClass("is-valid");
+    }
 
 
     var ctx = document.getElementById('myChart').getContext('2d');
@@ -69,8 +27,3 @@
         // Configuration options go here
         options: {}
     });
-
-
-
-
-  })(jQuery); // End of use strict
